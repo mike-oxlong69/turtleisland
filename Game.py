b8e8f1c6855c2406f05b56d6afad4d80b7527ee4
mike-oxlong69 <89999192+mike-oxlong69@users.noreply.github.com>
2021-10-11 22:51:31 +0000

    Created opening sequence and basic variables

--- Game.py
+++ Game.py
@@ -16,4 +16,13 @@ def slow(text):
     for char in words:
         sleep(0.25)
         print(char, end="", flush=True)
+# Start of menu player chooses name and is introduced to the game
+print("Text-Based Adventure Menu")
+print(slow("Welcome player - "))
+name = input("What should we call you? ")
+print("Alright, " + name + " choose your next move: ")
+# Actions and directions possible
+possible_actions = ["Sleep", "Study", "Explore", "Eat", "Quit"]
+possible_directions = ["north", "east", "south", "west"]
+
 
