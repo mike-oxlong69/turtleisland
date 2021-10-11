49046c975da83aae4b7b7d10c07b36cd15f1153c
mike-oxlong69 <89999192+mike-oxlong69@users.noreply.github.com>
2021-10-11 22:49:52 +0000

    Created 'slow' function for

--- Game.py
+++ Game.py
@@ -1,1 +1,19 @@
-print("Gay Anas")
+# Course: CS 30
+# Period: 3
+# Date created: September 21st, 2021
+# Date modified: September 28th, 2021
+# Name: Muhammad Munshi
+# Description: Menu for Text-Based Adventure
+import sys
+from time import sleep
+
+
+# Slow text for unique visual opening text
+def slow(text):
+    """ Prints title of game in a typewriter style """
+    words = str(text)
+
+    for char in words:
+        sleep(0.25)
+        print(char, end="", flush=True)
+
